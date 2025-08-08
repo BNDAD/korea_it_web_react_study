e4620d520820314ff532d8a23e6768c2cf5ab184
BNDAD <ral7057@naver.com>
2025-08-08 21:47:42 +0900

    Feat: state

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import BasicJsx from './components/BasicJSX/BasicJsx'
 import BasicProps from './components/BasicProps/BasicProps'
 import BasicReact from './components/BasicReact/BasicReact'
+import Calculator from './components/BasicState/Calculator/Calculator'
 import CountState from './components/BasicState/CountState/CountState'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
    {/* <BasicJsx /> */}
   {/* <BasicProps /> */}
   <CountState />
+  {/* <Calculator /> */}
    </>
   )
 }
